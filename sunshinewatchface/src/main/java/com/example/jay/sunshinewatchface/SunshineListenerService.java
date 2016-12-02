c15083c7f779a4ee1ab9879c8e5e4a21a469f99d
Jay <j.trollkarl@gmail.com>
2016-12-02 11:23:10 +0100

    working commit
    - added wearablelistenerservice (SunshineListenerService) to listen to
    incoming data items
    - updated wearable manifest to be able to filter data items

---
+++ sunshinewatchface/src/main/java/com/example/jay/sunshinewatchface/SunshineListenerService.java
@@ -0,0 +1,12 @@
+package com.example.jay.sunshinewatchface;
+
+import com.google.android.gms.wearable.DataEventBuffer;
+import com.google.android.gms.wearable.WearableListenerService;
+
+public class SunshineListenerService extends WearableListenerService {
+
+    @Override
+    public void onDataChanged(DataEventBuffer dataEventBuffer) {
+        super.onDataChanged(dataEventBuffer);
+    }
+}
